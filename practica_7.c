04bb46c2c9a1bfb05d466c7c6d124200c5e1497b
Artem <artem.egorov.art2006@gmail.com>
2023-11-16 20:03:41 +0300

    Сделал 7 и 8 практики

--- practica_7.c
+++ practica_7.c
@@ -1,2 +1,29 @@
 #include <ctype.h>
-#include <stralign.h>
+#include <locale.h>
+#include <stdio.h>
+//7. Сделать большими только гласные буквы;
+
+//Ищет гласные буквы и переводит их в верхний регистр
+void convertVolowesToUpper(char* str) {
+    while (*str)
+    {
+        if (*str == 'a' || *str == 'e' || *str == 'i' || *str == 'o' || *str == 'u' || *str == 'y')
+        {
+            *str = toupper(*str);
+        }
+        str++;
+    }
+    
+}
+
+int main() {
+    char *locale = setlocale(LC_ALL, "ru_RU.utf8");
+
+    char str[] = "Hello, World!";
+
+    convertVolowesToUpper(str);
+
+    printf("Результат: %s\n", str);
+
+    return 0;
+}
---
+++ practica_8.c
@@ -0,0 +1,29 @@
+//7. Найти среднее геометрическое всех чисел в массиве.
+
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+//Произведение элементов массива рекурсивно
+int multyOfArray(int *arr, int index, int length) {
+    if (index < length)
+    { 
+        index++;
+        return *arr * multyOfArray(arr + 1, index, length);
+    }
+    return 1;
+    
+} 
+
+int main() {
+    char *locale = setlocale(LC_ALL, "ru_RU.utf8");
+
+    int length = 8;
+    int arr[8] = {1, 2, 3, 4, 5, 6, 7, 10};
+
+    //pow(number, 1/n) - корень n-ой степени из nгmber
+    float gemetricalProgression = pow(multyOfArray(arr, 0, length), 1.0 / length);
+
+    printf("Ответ : %f", gemetricalProgression);
+
+    return 0;
+}
